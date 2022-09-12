b8bbf3fbeecf9f671f9cb517ee9677c4dbfc41ec
roshanikk <roshani.khairnar@aveshasystems.com>
2022-09-12 12:16:56 +0530

    feat(): added function to install dashboard

--- pkg/internal/install-kubeslice-enterprise.go
+++ pkg/internal/install-kubeslice-enterprise.go
@@ -8,16 +8,10 @@ import (
 	"github.com/kubeslice/slicectl/util"
 )
 
-const (
-	UIValuesFileName = "helm-values-UI.yaml"
-)
-
 func InstallKubeSliceUI(ApplicationConfiguration *ConfigurationSpecs) {
 	util.Printf("\nInstalling KubeSlice dashboard...")
 	cc := ApplicationConfiguration.Configuration.ClusterConfiguration
 	hc := ApplicationConfiguration.Configuration.HelmChartConfiguration
-	generateUIValuesFile(cc.ControllerCluster, ApplicationConfiguration.Configuration.HelmChartConfiguration.ImagePullSecret)
-	util.Printf("%s Generated Helm Values file for dashboard Installation %s", util.Tick, UIValuesFileName)
 	time.Sleep(200 * time.Millisecond)
 
 	installKubeSliceUI(cc.ControllerCluster, hc)
@@ -31,13 +25,10 @@ func InstallKubeSliceUI(ApplicationConfiguration *ConfigurationSpecs) {
 
 }
 
-func generateUIValuesFile(cluster Cluster, imagePullSecret ImagePullSecrets) {
-	util.DumpFile(fmt.Sprintf(controllerValuesTemplate+generateImagePullSecretsValue(imagePullSecret), cluster.ControlPlaneAddress), kubesliceDirectory+"/"+controllerValuesFileName)
-}
-
 func installKubeSliceUI(cluster Cluster, hc HelmChartConfiguration) {
 	args := make([]string, 0)
-	args = append(args, "--kube-context", cluster.ContextName, "--kubeconfig", cluster.KubeConfigPath, "upgrade", "-i", "kubeslice-ui", fmt.Sprintf("%s/%s", hc.RepoAlias, hc.UIChart.ChartName), "--namespace", "kubeslice-controller", "-f", kubesliceDirectory+"/"+UIValuesFileName)
+	fmt.Println("roshani", hc.UIChart.ChartName)
+	args = append(args, "--kube-context", cluster.ContextName, "--kubeconfig", cluster.KubeConfigPath, "upgrade", "-i", "kubeslice-ui", fmt.Sprintf("%s/%s", hc.RepoAlias, hc.UIChart.ChartName), "--namespace", "kubeslice-controller")
 	if hc.ControllerChart.Version != "" {
 		args = append(args, "--version", hc.ControllerChart.Version)
 	}
